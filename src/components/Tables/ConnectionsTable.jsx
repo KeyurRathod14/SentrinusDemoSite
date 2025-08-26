f4fe5965ed086ec04be5ef66a80498cd09e315ec
KeyurRathod14 <keyur.rathod@corslab.com>
2025-08-26 16:07:48 +0530

    feat: add Requests tab navigation and table with dummy data
    - modified Requests.jsx to include tab-based navigation i.e Connections and Access
    - created ConnectionsTable.jsx and AccessTable.jsx to display alerts in a table format
    - added ConnectionsData.js and AccessData.js to provide dummy data for connections and access

---
+++ src/components/Tables/ConnectionsTable.jsx
@@ -0,0 +1,121 @@
+import { connections } from "@/dummyData/ConnectionsData";
+import { Avatar, Checkbox, Table } from "antd";
+import { ReceiptText, ShieldCheck, ShieldX, Trash } from "lucide-react";
+import ButtonWithIcon from "../ButtonWithIcon";
+
+export default function ConnectionsTable() {
+  const columns = [
+    {
+      title: <Checkbox />,
+      dataIndex: "checkbox",
+      key: "checkbox",
+      render: () => <Checkbox />,
+      width: 50,
+    },
+    {
+      title: "Date",
+      dataIndex: "date",
+      key: "date",
+      render: (text) => <span className="text-TextGray">{text}</span>,
+      width: 110,
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text, record) => (
+        <div className="flex items-center gap-2">
+          <Avatar style={{ backgroundColor: "#87d068" }}>
+            {record.initials}
+          </Avatar>
+          <div className="flex flex-col leading-tight">
+            <span className="font-medium">{text}</span>
+            <span className="text-xs text-TextGray">{record.email}</span>
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "Device",
+      dataIndex: "device",
+      key: "device",
+      render: (text) => <span className="font-medium">{text}</span>,
+      width: 180,
+    },
+    {
+      title: "Agent Version",
+      dataIndex: "agentVersion",
+      key: "agentVersion",
+      render: (text) => <span className="text-TextGray">{text}</span>,
+      width: 150,
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      key: "actions",
+      render: () => (
+        <div className="flex items-center gap-3">
+          <ButtonWithIcon
+            icon={ShieldCheck}
+            text="Accept"
+            className="bg-green-100 text-green-600"
+          />
+          <ButtonWithIcon
+            icon={ShieldX}
+            text="Reject"
+            className="bg-red-100 text-red-600"
+          />
+        </div>
+      ),
+      width: 250,
+    },
+    {
+      title: "",
+      key: "Details",
+      render: () => (
+        <ReceiptText
+          size={18}
+          className="text-TextGray cursor-pointer hover:text-blue-700"
+        />
+      ),
+      width: 80,
+    },
+  ];
+
+  return (
+    <>
+      <div className="flex items-center justify-between p-5 border-2 border-BorderGray rounded-t-md">
+        <h1 className="font-semibold tracking-tighter text-2xl">Connections</h1>
+        <div className="flex justify-end gap-2 w-1/2">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-64 px-3 py-1.5 text-sm border-2 border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-gray-400"
+          />
+          <ButtonWithIcon
+            icon={ShieldCheck}
+            text="Accept"
+            className="bg-green-100 text-green-600"
+          />
+          <ButtonWithIcon
+            icon={ShieldX}
+            text="Reject"
+            className="bg-red-100 text-red-600"
+          />
+        </div>
+      </div>
+
+      <Table
+        className="custom-table"
+        columns={columns}
+        dataSource={connections}
+        rowKey="key"
+        pagination={{
+          pageSize: 5,
+          showSizeChanger: true,
+          pageSizeOptions: ["5", "10", "20"],
+        }}
+      />
+    </>
+  );
+}
--- src/pages/Requests.jsx
+++ src/pages/Requests.jsx
@@ -1,3 +1,25 @@
+import AccessTable from "@/components/Tables/AccessTable";
+import ConnectionsTable from "@/components/Tables/ConnectionsTable";
+import { Tabs } from "antd";
+
 export default function Requests() {
-  return <div>Welcome to the Requests Page!</div>;
+  const items = [
+    {
+      key: "1",
+      label: "Connections",
+      children: <ConnectionsTable />,
+    },
+    {
+      key: "2",
+      label: "Access",
+      children: <AccessTable />,
+    },
+  ];
+  return (
+    <>
+      <div className="px-4">
+        <Tabs defaultActiveKey="1" items={items} className="!font-inter" />
+      </div>
+    </>
+  );
 }
